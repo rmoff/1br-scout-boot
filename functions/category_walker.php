3ef281e8b98b12d5a389da1a15e5ded7c5748e60
Callum Smith <callum@smither.org>
2022-03-09 23:38:44 +0000

    Update functions/category_walker.php

--- functions/category_walker.php
+++ functions/category_walker.php
@@ -94,7 +94,7 @@ class wpsb84532_category_walker extends Walker {
             $is_selected = in_array( $category->term_id, $args['selected_cats'], true );
             $is_disabled = ! empty( $args['disabled'] );
  
-            $output .= "\n<div class='ml-".$depth."' id='{$taxonomy}-{$category->term_id}'$class>" .
+            $output .= "\n<div style='margin-left:".$depth."em;' id='{$taxonomy}-{$category->term_id}'$class>" .
                 '<label class="selectit"><input value="' . $category->term_id . '" type="checkbox" name="' . $name . '[]" id="in-' . $taxonomy . '-' . $category->term_id . '"' .
                 checked( $is_selected, true, false ) .
                 disabled( $is_disabled, true, false ) . ' /> ' .
@@ -117,6 +117,6 @@ class wpsb84532_category_walker extends Walker {
      * @param array   $args        An array of arguments. @see wp_terms_checklist()
      */
     public function end_el( &$output, $data_object, $depth = 0, $args = array() ) {
-        // $output .= "</li>\n";
+        $output .= "</div>\n";
     }
 }
