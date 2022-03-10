6054f1c825f426a73f637f5aaab13bfb73499b8b
Callum Smith <callum@smither.org>
2022-03-10 03:21:14 +0000

    Move recent posts in sidebar to bottom

--- sidebar.php
+++ sidebar.php
@@ -26,7 +26,7 @@
 				<?php
 				foreach ($posts as $post){
 					?>
-					<div class="card">
+					<div class="card p-1">
 					<p><a href=<?php echo '"'.get_permalink($post).'"'; ?>><?php echo $post->post_title;  ?></a></br>
 					<?php echo get_the_excerpt($post); ?> <a href=<?php echo '"'.get_permalink($post).'"'; ?>>Read more</a></p>
 					</div>
