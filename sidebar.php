dc1464fe6c1d17406a7e6f0c7a8d5c2c6b95f6aa
Callum Smith <callum@smither.org>
2022-03-10 03:28:48 +0000

    Move recent posts in sidebar to bottom

--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,7 @@
 ?>
 
 <?php if( is_active_sidebar('sidebar-widget-area') ): ?>
-<div id="sidebar" class="sidebar col-lg-4 pull-md-8" role="navigation">
+<div id="sidebar" class="sidebar col-lg-4 d-sm-none" role="navigation">
 	<?php
 		b4st_sidebar_before();
 		$post=get_post();
